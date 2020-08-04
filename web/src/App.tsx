fa284e31303d7bf294841b253651cdeb5055d995
marianefelix <marianefelix59@gmail.com>
2020-08-04 09:53:06 -0300

    Criação das rotas da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Routes from './routes';
+
 import './assets/styles/global.css';
-import Landing from './pages/Landing';
+
 
 function App() {
   return (
-    <Landing/>
+    <Routes />
   );
 }
 
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -23,14 +25,14 @@ function Landing(){
                     className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
