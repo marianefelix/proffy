fda91599b5199bdb12d03599a2d3753ca89f5f38
marianefelix <marianefelix59@gmail.com>
2020-08-05 13:56:04 -0300

    Foram criadas as tabelas class, class schedule e connections

---
+++ server/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,24 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary();
+        
+        //id do proffy que entrou em contato
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+
+        //quando entrou em contato
+        table.timestamp('created_at')
+            .defaultTo('now()')
+            .notNullable();
+    });
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('connections');
+}
