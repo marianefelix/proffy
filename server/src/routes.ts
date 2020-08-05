7fbf0e52e3600efed650b89af2dad433d09c41a2
marianefelix <marianefelix59@gmail.com>
2020-08-05 16:13:06 -0300

    Criada a rota para registrar aula

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,79 @@
 import express from 'express';
+import db from './database/connection';
+import covertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
+
+routes.post('/classes', async (request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+
+    const trx = await db.transaction();
+
+    try{
+
+        //insere usuario
+        const insertedUsersIds = await trx('users').insert({
+            name, 
+            avatar, 
+            whatsapp,
+            bio,
+        });
+    
+        //recupera id do usuario registrado
+        const user_id = insertedUsersIds[0];
+    
+        //insere aula
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+    
+        //recupera id da aula registrada
+        const class_id = insertedClassesIds[0];
+    
+        //percorre o array de objetos passado
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: covertHourToMinutes(scheduleItem.from),
+                to: covertHourToMinutes(scheduleItem.to),
+            };
+        })
+    
+        //insere horario de aula
+        await trx('class_schedule').insert(classSchedule);
+    
+        //realiza as alteracoes no banco em apenas uma transacao, caso todas sejam sucesso
+        await trx.commit();
+    
+        //retorna criado com sucesso
+        return response.status(201).send();
+
+    } catch(err){
+        //desfaz qualquer alteracao feita no banco
+        await trx.rollback();
+
+        return response.status(400).json({
+            error: 'Unexpecting error while creating new class'
+        })
+    }
+
 });
 
 export default routes;
---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,8 @@
+export default function covertHourToMinutes(time: string){
+    //separa a string em hora e minutos 
+    const [hour, minutes] = time.split(':').map(Number);
+    //converte a hora para minutos
+    const timeInMinutes = (hour * 60) + minutes;
+    
+    return timeInMinutes;
+}
