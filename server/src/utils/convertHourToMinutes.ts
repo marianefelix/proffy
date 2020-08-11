b3ee9a690c8c4e3d8548ac5eb3617046ea7c6d7f
marianefelix <marianefelix59@gmail.com>
2020-08-11 15:11:43 -0300

    Config deploy web

--- server/src/utils/convertHourToMinutes.ts
+++
@@ -1,8 +0,0 @@
-export default function convertHourToMinutes(time: string){
-    //separa a string em hora e minutos 
-    const [hour, minutes] = time.split(':').map(Number);
-    //converte a hora para minutos
-    const timeInMinutes = (hour * 60) + minutes;
-    
-    return timeInMinutes;
-}
